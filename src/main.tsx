a77012b04d3d499f37e63349cc29c04e27b57d81
anshugusain482-droid <anshugusain482@gmail.com>
2026-06-18 12:21:46 +0530

    Optimize smooth scrolling

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,22 @@
+import { createRoot } from "react-dom/client";
+import App from "./app/App.tsx";
+import "./styles/index.css";
+import Lenis from "lenis";
+import "lenis/dist/lenis.css";
 
-  import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+createRoot(document.getElementById("root")!).render(<App />);
 
-  createRoot(document.getElementById("root")!).render(<App />);
-  
+const lenis = new Lenis({
+  duration: 0.65,
+  easing: (t: number) => 1 - Math.pow(1 - t, 3),
+  smoothWheel: true,
+  wheelMultiplier: 1,
+  touchMultiplier: 1,
+});
+
+function raf(time: number) {
+  lenis.raf(time);
+  requestAnimationFrame(raf);
+}
+
+requestAnimationFrame(raf);
